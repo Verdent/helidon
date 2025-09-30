326ff609371b178fa22a673ad075ef601125631d
David Kral <david.k.kral@oracle.com>
2025-09-30 14:10:12 +0200

    fixes after rebase
    Signed-off-by: David Kral <david.k.kral@oracle.com>

---
+++ http/media/helison/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module helidon.http.media.helison {
+    requires io.helidon.builder.api;
+    requires io.helidon.common.config;
+    requires io.helidon.http.media;
+    requires helidon.json.binding;
+}

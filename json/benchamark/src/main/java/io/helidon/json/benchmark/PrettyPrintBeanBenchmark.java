dd427d8130c2f69fbaaa00f9f76e91e83dce3650
David Kral <david.k.kral@oracle.com>
2025-10-14 15:40:52 +0200

    tmp
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- json/benchamark/src/main/java/io/helidon/json/benchmark/PrettyPrintBeanBenchmark.java
+++ json/benchamark/src/main/java/io/helidon/json/benchmark/PrettyPrintBeanBenchmark.java
@@ -38,7 +38,7 @@ public class PrettyPrintBeanBenchmark {
             }""";
 
     private static final ObjectMapper BASIC_JACKSON = new ObjectMapper();
-    private static final ObjectMapper JACKSON_BACKBIRD = new ObjectMapper().registerModule(new BlackbirdModule());
+    private static final ObjectMapper JACKSON_BLACKBIRD = new ObjectMapper().registerModule(new BlackbirdModule());
 
     static {
         //To enable field name processing as hashes
@@ -57,7 +57,7 @@ public class PrettyPrintBeanBenchmark {
 
     @Benchmark
     public void jacksonBlackbird(Blackhole bh) throws JsonProcessingException {
-        bh.consume(JACKSON_BACKBIRD.readValue(MY_JAVA_BEAN_WITH_OTHER_BEAN, MyJavaBean.class));
+        bh.consume(JACKSON_BLACKBIRD.readValue(MY_JAVA_BEAN_WITH_OTHER_BEAN, MyJavaBean.class));
     }
 
     @Benchmark

b8af3587b4661a9251893afaf73dcdd979138bb1
Tomas Langer <tomas.langer@oracle.com>
2025-12-22 15:20:29 +0100

    Avoid generating a dead-local-store config if no configured service/provider. Removed associated spotbugs excludes

--- builder/codegen/src/main/java/io/helidon/builder/codegen/GenerateAbstractBuilder.java
+++ builder/codegen/src/main/java/io/helidon/builder/codegen/GenerateAbstractBuilder.java
@@ -476,7 +476,7 @@ final class GenerateAbstractBuilder {
         if (hasProvider || hasRegistryService) {
             boolean configured = prototypeInfo.configured().isPresent();
 
-            if (configured) {
+            if (configured && hasConfiguredRegistryServiceOrProvider(options)) {
                 // need to have a non-null config instance
                 preBuildBuilder.addContent("var config = config().map(")
                         .addContent(CONFIG)
@@ -527,6 +527,20 @@ final class GenerateAbstractBuilder {
                                                             Utils.options(options),
                                                             preBuildBuilder));
         classBuilder.addMethod(preBuildBuilder);
+    }
+
+    private static boolean hasConfiguredRegistryServiceOrProvider(List<OptionHandler> options) {
+        boolean configured = options.stream()
+                .map(OptionHandler::option)
+                .filter(OptionInfo::registryService)
+                .anyMatch(it -> it.configured().isPresent());
+        if (configured) {
+            return true;
+        }
+        return options.stream()
+                .map(OptionHandler::option)
+                .filter(it -> it.provider().isPresent())
+                .anyMatch(it -> it.configured().isPresent());
     }
 
     private static boolean hasRegistryService(List<OptionHandler> options) {
